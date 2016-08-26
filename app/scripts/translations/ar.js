5918d2b86591f1af805f58c4b58cf5e5053a91eb
badr bellaj <bellaj.badr@gmail.com>
2016-08-26 11:18:32 +0100

    Create ar.js
    arabic translation

---
+++ app/scripts/translations/ar.js
@@ -0,0 +1,12 @@
+// Arabic
+'use strict';
+var ar = function() {}
+ar.code = 'ar';
+ar.data = {
+
+  /* New */
+  CX_error_1:           '["Add Wallet"](/cx-wallet.html#add-wallet) لا يوجد لديك محفظة مسجلة .المرجو النقر على',
+  CX_quicksend:         'أرسال سريع', 
+  SEND_TransferTotal:   'نقل الرصيد الكلي',  / 
+  FOOTER_1b:            'أنشأه ',
+  }
