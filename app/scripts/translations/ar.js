afdf9dcac6be9b9bbee64700b5c920b51f3d072c
badr bellaj <bellaj.badr@gmail.com>
2016-08-26 11:31:15 +0100

    Update ar.js

--- app/scripts/translations/ar.js
+++ app/scripts/translations/ar.js
@@ -7,7 +7,7 @@ ar.data = {
   /* New */
   CX_error_1:           '["Add Wallet"](/cx-wallet.html#add-wallet) لا يوجد لديك محفظة مسجلة .المرجو النقر على',
   CX_quicksend:         'أرسال سريع', 
-  SEND_TransferTotal:   'نقل الرصيد الكلي',  / 
+  SEND_TransferTotal:   'نقل الرصيد الكلي ' , 
   FOOTER_1b:            'أنشأه ',
   
   
